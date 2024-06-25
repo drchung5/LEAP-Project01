5ddcd634018bdd3fc47372e3d26dee92eb14284e
David Chung <dchung@dchung.com>
2024-06-25 13:04:02 -0500

    initial commit

---
+++ app/src/main/java/com/example/starwars/pages/HomePage.kt
@@ -0,0 +1,31 @@
+package com.example.starwars.pages
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.starwars.R
+
+@Composable
+fun HomePage() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.starwars),
+            contentDescription = "PNG Image",
+            modifier = Modifier.fillMaxSize().padding(1.dp)
+        )
+    }
+}
