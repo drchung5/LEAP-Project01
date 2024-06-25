536bb51a869da082630fe4ce4ad7da5a40b0fc4f
David Chung <dchung@dchung.com>
2024-06-25 13:19:59 -0500

    Removed unused import

--- app/src/main/java/com/example/starwars/MainActivity.kt
+++ app/src/main/java/com/example/starwars/MainActivity.kt
@@ -31,7 +31,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
